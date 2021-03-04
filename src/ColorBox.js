4cf260c27058359fb2166398913c9320dbc724b5
Jacob Diaz <hello@jacobdiaz.dev>
2021-03-03 23:49:13 -0600

    Added Copy Animation

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,16 +2,47 @@ import React, { Component } from "react";
 import "./ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 export default class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      copied: false,
+    };
+
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+
+  changeCopyState() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 800);
+    });
+  }
+
   render() {
     // Grab our props!
     const { name, background } = this.props;
+    const { copied } = this.state;
     return (
       // The text for copy clipboard is what is being copied
-      <CopyToClipboard text={background}>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+        {/*Color Box */}
         <div
           className="color-box"
           style={{ background: this.props.background }}
         >
+          {/* Overlay  */}
+          <div
+            // When Copied is true... set the classname to copy-overlay.show
+            className={`copy-overlay ${copied && "show"}`}
+            style={{ background: background }}
+          ></div>
+
+          <div className={`copy-msg ${copied && "show"}`}>
+            <h1> COPIED</h1>
+            <p>{background}</p>
+          </div>
+
+          {/* Bottom Bar */}
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
